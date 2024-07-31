2c349efb505895995ede78958cafe64346b8d3bd
Ayan Chattejee <ayan25844@gmail.com>
2024-07-31 14:52:57 +0530

    Update Inventory.java

--- week 1/Week 1_Algorithms_Data Structures/Inventory Management System/Inventory.java
+++ week 1/Week 1_Algorithms_Data Structures/Inventory Management System/Inventory.java
@@ -1,7 +1,8 @@
+// Q1. Explain why data structures and algorithms are essential in handling large inventories.
+// A1. Data structures and algorithms are essential for managing large inventories because they enable efficient storage, retrieval, and manipulation of data.
 
-// 1. Data structures and algorithms are essential for managing large inventories because they enable efficient storage, retrieval, and manipulation of data.
-
-// 2. In a large inventory the amount of data will be huge. To efficiently access and manipulate them, we need a proper structure to store data so that the data can be maintained easily. If we don't structure them properly, due to the huge size of the data, it will take a lot of time to search, create, update and delete them.
+// Q2. Discuss the types of data structures suitable for this problem.
+// A2. In a large inventory the amount of data will be huge. To efficiently access and manipulate them, we need a proper structure to store data so that the data can be maintained easily. If we don't structure them properly, due to the huge size of the data, it will take a lot of time to search, create, update and delete them.
 
 import java.util.HashMap;
 
@@ -27,9 +28,11 @@ public class Inventory {
         System.out.println();
     }
 
+    // Q3. Analyze the time complexity of each operation (add, update, delete) in your chosen data structure.
+
     public void add(Product product) {
 
-        // 3. This will simply create a new product and due to that the autoGenID will
+        // A3. This will simply create a new product and due to that the autoGenID will
         // be incremented, then we will simply map the autoGenID to the new Product.
         // This will be done in constant time complexity O(1)
 
@@ -40,7 +43,7 @@ public class Inventory {
 
     public static void update(int id, String newName, int newQuantity, double newPrice) {
 
-        // 3. This will find the product using product_id. If it exists then it will be
+        // A3. This will find the product using product_id. If it exists then it will be
         // updated else the else block will be executed. To access the product, the time
         // complexity will be O(1)
 
@@ -53,7 +56,7 @@ public class Inventory {
 
     public static void delete(int id) {
 
-        // 3. This will find the product using product_id. If it exists then it will be
+        // A3. This will find the product using product_id. If it exists then it will be
         // updated else the else block will be executed. To access the product, the time
         // complexity will be O(1)
 
