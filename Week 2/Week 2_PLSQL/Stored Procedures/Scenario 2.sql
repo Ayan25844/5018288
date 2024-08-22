37be89a50876097418cc26cbd3b4a5e1da781b47
Ayan Chattejee <ayan25844@gmail.com>
2024-08-22 16:29:37 +0530

    Update Scenario 2.sql

--- Week 2/Week 2_PLSQL/Stored Procedures/Scenario 2.sql
+++ Week 2/Week 2_PLSQL/Stored Procedures/Scenario 2.sql
@@ -1,16 +1,6 @@
--- Write a stored procedure UpdateEmployeeBonus that updates the salary of employees in a given department by adding a bonus percentage passed as a parameter.
-
-CREATE OR REPLACE PROCEDURE UPDATEEMPLOYEEBONUS (
-    P_DEPARTMENT IN VARCHAR2,
-    P_BONUS_PERCENTAGE IN NUMBER
-) IS
+CREATE OR REPLACE PROCEDURE UpdateEmployeeBonus(DEPT IN EMPLOYEES.DEPARTMENT%TYPE,BONUS IN NUMBER) IS
 BEGIN
     UPDATE EMPLOYEES
-    SET
-        SALARY = SALARY + (
-            SALARY * P_BONUS_PERCENTAGE / 100
-        )
-    WHERE
-        DEPARTMENT = P_DEPARTMENT;
-    COMMIT; -- Commit the changes, if desired
+    SET SALARY=FLOOR(SALARY+(SALARY*(BONUS/100)))
+    WHERE DEPARTMENT=DEPT;
 END;
