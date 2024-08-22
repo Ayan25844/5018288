f27214ec41aa66fa159d03833d402aea24ce8fee
Ayan Chattejee <ayan25844@gmail.com>
2024-08-23 00:30:00 +0530

    Update Auditor.java

--- Week 3/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/model/Auditor.java
+++ Week 3/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/model/Auditor.java
@@ -1,1 +1,11 @@
+package com.example.EmployeeManagementSystem.model;
 
+import java.util.Optional;
+import org.springframework.data.domain.AuditorAware;
+
+public class Auditor implements AuditorAware<String> {
+
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("Ayan Chatterjee");
+    }
+}
