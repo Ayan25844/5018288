8108d4e5af311ef905ce22953589a4fea1b7a240
Ayan Chattejee <ayan25844@gmail.com>
2024-08-22 16:15:55 +0530

    Update Test.java

--- week 1/Week 1_Design Patterns and Principles/ProxyPatternExample/Test.java
+++ week 1/Week 1_Design Patterns and Principles/ProxyPatternExample/Test.java
@@ -1,22 +1,15 @@
-public class Test {
+package ProxyPatternExample;
+
+public class Test_6 {
+
     public static void main(String[] args) {
 
-        Image image1 = new ProxyImage("test_image1.jpg");
-        Image image2 = new ProxyImage("test_image2.jpg");
+        Image image = new ProxyImage();
 
-        // Image will be loaded from disk
-        image1.display();
-        System.out.println();
+        image.display("Kill");
+        image.display("Stree_2");
+        image.display("Stree_2");
 
-        // Image will not be loaded from disk, as it is already cached
-        image1.display();
-        System.out.println();
-
-        // Image will be loaded from disk
-        image2.display();
-        System.out.println();
-
-        // Image will not be loaded from disk, as it is already cached
-        image2.display();
     }
+
 }
