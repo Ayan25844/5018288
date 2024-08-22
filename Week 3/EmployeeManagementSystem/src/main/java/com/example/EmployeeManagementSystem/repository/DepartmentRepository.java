21b9eaf8d0d1954e169a4b5d6eb57e204fc0683b
Ayan Chattejee <ayan25844@gmail.com>
2024-08-23 00:16:34 +0530

    Update DepartmentRepository.java

--- Week 3/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/repository/DepartmentRepository.java
+++ Week 3/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/repository/DepartmentRepository.java
@@ -1,33 +1,19 @@
 package com.example.EmployeeManagementSystem.repository;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.ArrayList;
+import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import com.example.EmployeeManagementSystem.model.Employee;
 import com.example.EmployeeManagementSystem.model.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.EmployeeManagementSystem.projection.DepartmentProjection;
 
+@Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
+    public ArrayList<DepartmentProjection> findAllBy();
+
     @Query(name = "Department.findByName")
-    Department findByName(@Param("name") String name);
-
-    @Query(name = "Department.findDepartmentsWithEmployeeCountGreaterThan")
-    List<Department> findDepartmentsWithEmployeeCountGreaterThan(@Param("minEmployeeCount") Long minEmployeeCount);
-
-    Page<Employee> findByDepartmentName(String departmentName, Pageable pageable);
-
-    Optional<Employee> findByEmail(String email);
-
-    @Query("SELECT d.id AS id, d.name AS name, " +
-            "(SELECT COUNT(e) FROM Employee e WHERE e.department.id = d.id) AS employeeCount " +
-            "FROM Department d")
-    List<DepartmentProjection> findAllDepartmentProjections();
+    public ArrayList<Department> findByName(@Param("name") String name);
 
 }
