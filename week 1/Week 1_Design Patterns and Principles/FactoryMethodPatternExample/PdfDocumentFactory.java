41cf459e6b6cf0aaf99a4b4dd870ff63fb986a84
Ayan Chattejee <ayan25844@gmail.com>
2024-08-22 16:09:09 +0530

    Update PdfDocumentFactory.java

--- week 1/Week 1_Design Patterns and Principles/FactoryMethodPatternExample/PdfDocumentFactory.java
+++ week 1/Week 1_Design Patterns and Principles/FactoryMethodPatternExample/PdfDocumentFactory.java
@@ -1,10 +1,7 @@
 public class PdfDocumentFactory extends DocumentFactory {
 
-    @Override
-    public void createDocument() {
-        PdfDocument document = new PdfDocument();
-        document.open();
-        document.save();
-        document.close();
+    public Document createDocument() {
+        return new PdfDocument();
     }
+
 }
