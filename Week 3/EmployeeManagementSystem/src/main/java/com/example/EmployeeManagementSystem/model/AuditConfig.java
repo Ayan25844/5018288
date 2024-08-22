e953670777c891bdeb028a6cd218e7082d40c53e
Ayan Chattejee <ayan25844@gmail.com>
2024-08-23 00:31:16 +0530

    Update AuditConfig.java

--- Week 3/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/model/AuditConfig.java
+++ Week 3/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/model/AuditConfig.java
@@ -1,1 +1,16 @@
+package com.example.EmployeeManagementSystem.model;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return new Auditor();
+    }
+}
