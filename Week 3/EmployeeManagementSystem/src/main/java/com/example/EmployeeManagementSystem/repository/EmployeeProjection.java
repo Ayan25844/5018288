a5fb8fa783e030bdf4f7383f24f92cc248081264
Ayan Chattejee <ayan25844@gmail.com>
2024-08-23 00:20:38 +0530

    Update EmployeeProjection.java

--- Week 3/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/repository/EmployeeProjection.java
+++ Week 3/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/repository/EmployeeProjection.java
@@ -1,15 +1,18 @@
-package com.example.EmployeeManagementSystem.repository;
+package com.example.EmployeeManagementSystem.projection;
 
 import org.springframework.beans.factory.annotation.Value;
 
 public interface EmployeeProjection {
 
+    @Value("#{target.id}")
     Long getId();
 
-    String getName();
+    @Value("#{target.name}")
+    public String getName();
 
-    String getEmail();
+    @Value("#{target.email}")
+    public String getEmail();
 
     @Value("#{target.department.name}")
-    String getDepartmentName();
+    public String getDepartmentName();
 }
