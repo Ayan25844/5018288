1fe6b38d71a7bf8f8a34891af27233566fe2ded4
Ayan Chattejee <ayan25844@gmail.com>
2024-08-23 00:16:01 +0530

    Update EmployeeRepository.java

--- Week 3/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/repository/EmployeeRepository.java
+++ Week 3/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/repository/EmployeeRepository.java
@@ -1,29 +1,24 @@
 package com.example.EmployeeManagementSystem.repository;
 
-import java.util.List;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Pageable;
+import java.util.ArrayList;
+import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.EmployeeManagementSystem.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.EmployeeManagementSystem.projection.EmployeeProjection;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
+    ArrayList<EmployeeProjection> findAllBy();
+
+    @Query(name = "Employee.findByName")
+    public ArrayList<Employee> findByName(@Param("name") String name);
+
     @Query(name = "Employee.findByEmail")
-    Employee findByEmail(@Param("email") String email);
+    public ArrayList<Employee> findByEmail(@Param("email") String email);
 
-    @Query(name = "Employee.findByDepartmentName")
-    List<Employee> findByDepartmentName(@Param("departmentName") String departmentName);
-
-    Page<Employee> findAll(Pageable pageable);
-
-    List<Employee> findAll(Sort sort);
-
-    @Query("SELECT e.id AS id, e.name AS name, e.email AS email, e.department.name AS departmentName " +
-            "FROM Employee e WHERE e.department.name = :departmentName")
-    List<EmployeeProjection> findEmployeeProjectionsByDepartmentName(@Param("departmentName") String departmentName);
+    @Query(name = "Employee.findByDepartmentId")
+    public ArrayList<Employee> findByDepartmentId(@Param("id") Long id);
 }
