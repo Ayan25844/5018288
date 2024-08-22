0954d4f26566c4fe3b23c471f4f3322b2b87e57a
Ayan Chattejee <ayan25844@gmail.com>
2024-08-23 00:10:24 +0530

    Update Employee.java

--- Week 3/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/model/Employee.java
+++ Week 3/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/model/Employee.java
@@ -1,53 +1,56 @@
 package com.example.EmployeeManagementSystem.model;
 
 import lombok.Data;
+import java.util.Date;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 
-import jakarta.persistence.*;
-
-import java.time.LocalDateTime;
-
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
-import org.hibernate.annotations.BatchSize;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-@Entity
-@Table(name = "employees")
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.EntityListeners;
+
 @Data
+@Entity
+@Table(name = "employeeDetails")
 @NamedQueries({
-        @NamedQuery(name = "Employee.findByEmail", query = "SELECT e FROM Employee e WHERE e.email = :email"),
-        @NamedQuery(name = "Employee.findByDepartmentName", query = "SELECT e FROM Employee e WHERE e.department.name = :departmentName")
+        @NamedQuery(name = "Employee.findByName", query = "SELECT e FROM Employee e WHERE e.name=:name"),
+        @NamedQuery(name = "Employee.findByEmail", query = "SELECT e FROM Employee e WHERE e.email=:email"),
+        @NamedQuery(name = "Employee.findByDepartmentId", query = "SELECT e FROM Employee e WHERE e.department.id=:id")
 })
+@EntityListeners(AuditingEntityListener.class)
 public class Employee {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
     private String name;
     private String email;
-
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "department_id")
-    @Fetch(FetchMode.SELECT)
-    @BatchSize(size = 10)
     private Department department;
 
-    @JoinColumn(name = "department_id")
-    private Department department_2;
-
+    @Temporal(TemporalType.TIMESTAMP)
     @CreatedDate
-    private LocalDateTime createdDate;
-
+    private Date createDate;
+    @CreatedBy
+    private String createdBy;
+    @Temporal(TemporalType.TIMESTAMP)
     @LastModifiedDate
-    private LocalDateTime lastModifiedDate;
-
+    private Date LastModifiedDate;
     @LastModifiedBy
-    @Column(nullable = false)
     private String lastModifiedBy;
 
-    @CreatedBy
-    @Column(nullable = false, updatable = false)
-    private String createdBy;
 }
