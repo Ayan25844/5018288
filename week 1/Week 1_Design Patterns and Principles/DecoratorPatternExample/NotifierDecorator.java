011acbf9db212a53403e5a9707725a2e17a66df7
Ayan Chattejee <ayan25844@gmail.com>
2024-08-22 16:02:05 +0530

    Update NotifierDecorator.java

--- week 1/Week 1_Design Patterns and Principles/DecoratorPatternExample/NotifierDecorator.java
+++ week 1/Week 1_Design Patterns and Principles/DecoratorPatternExample/NotifierDecorator.java
@@ -1,12 +1,17 @@
 public abstract class NotifierDecorator implements Notifier {
-    protected Notifier wrapped;
+
+    private final Notifier wrapped;
 
     public NotifierDecorator(Notifier wrapped) {
         this.wrapped = wrapped;
     }
 
-    @Override
-    public void send(String message) {
-        wrapped.send(message);
+    public void send(String msg) {
+        wrapped.send(msg);
     }
+
+    public String getUserName() {
+        return wrapped.getUserName();
+    }
+
 }
