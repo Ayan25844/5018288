069d059519b21ce1500390cc15ee6d7009207166
Ayan Chattejee <ayan25844@gmail.com>
2024-08-26 17:15:05 +0530

    Update GlobalExceptionHandler.java

--- Week 4 and 5/BookstoreAPI/src/main/java/com/example/BookstoreAPI/GlobalExceptionHandler.java
+++ Week 4 and 5/BookstoreAPI/src/main/java/com/example/BookstoreAPI/GlobalExceptionHandler.java
@@ -1,1 +1,34 @@
+package com.example.BookstoreAPI;
 
+import java.util.NoSuchElementException;
+import org.springframework.http.HttpStatus;
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.ResponseEntity;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = NoSuchElementException.class)
+    public ResponseEntity<?> notFound() {
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = EntityNotFoundException.class)
+    public ResponseEntity<?> noContent() {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @ExceptionHandler(value = BadRequestException.class)
+    public ResponseEntity<?> badRequest() {
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = HttpMessageNotReadableException.class)
+    public ResponseEntity<?> notReadable() {
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+}
