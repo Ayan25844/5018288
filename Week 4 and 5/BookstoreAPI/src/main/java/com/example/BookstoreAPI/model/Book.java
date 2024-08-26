e08679edf0080683846454bb99364f5740952a1e
Ayan Chattejee <ayan25844@gmail.com>
2024-08-26 17:08:04 +0530

    Update Book.java

--- Week 4 and 5/BookstoreAPI/src/main/java/com/example/BookstoreAPI/model/Book.java
+++ Week 4 and 5/BookstoreAPI/src/main/java/com/example/BookstoreAPI/model/Book.java
@@ -1,28 +1,33 @@
-package com.example.BookstoreAPI.model;
+package com.example.BookstoreAPI.entity;
 
+import lombok.Data;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.NamedQueries;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Version;
-import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "Book_Details")
+@NamedQueries({
+        @NamedQuery(name = "Book.getByIsbn", query = "SELECT b FROM Book b WHERE b.isbn=:isbn"),
+        @NamedQuery(name = "Book.getByPrice", query = "SELECT b FROM Book b WHERE b.price=:price"),
+        @NamedQuery(name = "Book.getByTitle", query = "SELECT b FROM Book b WHERE b.title=:title"),
+        @NamedQuery(name = "Book.getByAuthor", query = "SELECT b FROM Book b WHERE b.author=:author")
+})
 public class Book {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+    private Integer id;
+    private Integer price;
     private String title;
-
-    private String author;
-
-    private Double price;
-
+    @Column(nullable = false, unique = true)
     private String isbn;
-
-    @Version
-    private Integer version;
-
+    @Column(nullable = false)
+    private String author;
 }
