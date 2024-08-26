7c9bb26b19a57e3cc399dad1b1633cd968ae4f86
Ayan Chattejee <ayan25844@gmail.com>
2024-08-26 17:07:05 +0530

    Update CustomerDTO.java

--- Week 4 and 5/BookstoreAPI/src/main/java/com/example/BookstoreAPI/dto/CustomerDTO.java
+++ Week 4 and 5/BookstoreAPI/src/main/java/com/example/BookstoreAPI/dto/CustomerDTO.java
@@ -4,7 +4,8 @@ import lombok.Data;
 
 @Data
 public class CustomerDTO {
-    private Long id;
+
+    private Integer id;
     private String name;
-    private String email;
+    private String password;
 }
