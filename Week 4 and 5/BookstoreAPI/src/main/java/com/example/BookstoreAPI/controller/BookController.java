445f3201fdaf34c5b101bc7924c02e669906b072
Ayan Chattejee <ayan25844@gmail.com>
2024-08-26 17:04:59 +0530

    Update BookController.java

--- Week 4 and 5/BookstoreAPI/src/main/java/com/example/BookstoreAPI/controller/BookController.java
+++ Week 4 and 5/BookstoreAPI/src/main/java/com/example/BookstoreAPI/controller/BookController.java
@@ -1,70 +1,122 @@
 package com.example.BookstoreAPI.controller;
 
-import org.springframework.http.HttpHeaders;
+import java.util.NoSuchElementException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 import com.example.BookstoreAPI.dto.BookDTO;
-import com.example.BookstoreAPI.model.Customer;
+import org.springframework.data.domain.Sort;
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
+import jakarta.persistence.EntityNotFoundException;
 import com.example.BookstoreAPI.service.BookService;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/books")
+@RequestMapping("books")
 public class BookController {
 
-    private final BookService bookService;
-    private final BookResourceAssembler assembler;
+    @Autowired
+    private BookService bookService;
 
-    public BookController(BookService bookService, BookResourceAssembler assembler) {
-        this.bookService = bookService;
-        this.assembler = assembler;
+    @PostMapping
+    public ResponseEntity<?> createEntry(@RequestBody BookDTO entry) throws Exception {
+        try {
+            bookService.save(entry);
+        } catch (Exception e) {
+            throw new BadRequestException();
+        }
+        return new ResponseEntity<>(entry, HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity<?> getBooks(@RequestHeader(value = HttpHeaders.ACCEPT, defaultValue = "application/json") String acceptHeader) {
-        List<BookDTO> books = bookService.getAllBooks();
-
-        if (acceptHeader.equals("application/xml")) {
-            return ResponseEntity.ok()
-                    .body(books); 
+    public ResponseEntity<?> getAll() {
+        if (bookService.getAll() != null && !bookService.getAll().isEmpty()) {
+            return new ResponseEntity<>(bookService.getAll(), HttpStatus.OK);
         } else {
-            return ResponseEntity.ok()
-                    .body(books);
+            throw new NoSuchElementException();
         }
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<?> getBookById(@PathVariable Long id) {
-        BookDTO bookDTO = bookService.getBookById(id);
-        if (bookDTO == null) {
-            return ResponseEntity.notFound().build();
+    @GetMapping("page/{no}")
+    public ResponseEntity<?> getAll(@PathVariable int no) {
+        if (bookService.getAll() != null && !bookService.getAll().isEmpty()) {
+            Pageable pageable = PageRequest.of(no, 3);
+            return new ResponseEntity<>(bookService.getAll(pageable), HttpStatus.OK);
+        } else {
+            throw new NoSuchElementException();
         }
-        return ResponseEntity.ok(assembler);
     }
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<Void> addBook(@RequestBody BookDTO bookDTO) {
-        bookService.addBook(bookDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+    @GetMapping("sort/{field}")
+    public ResponseEntity<?> getAll(@PathVariable String field) {
+        if (bookService.getAll() != null && !bookService.getAll().isEmpty()) {
+            return new ResponseEntity<>(bookService.getAll(field), HttpStatus.OK);
+        } else {
+            throw new NoSuchElementException();
+        }
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Void> updateBook(@PathVariable Long id, @RequestBody BookDTO bookDTO) {
-        bookService.updateBook(id, bookDTO);
-        return ResponseEntity.ok().build();
+    @GetMapping("sort/{field}/page/{no}")
+    public ResponseEntity<?> getAll(@PathVariable int no, @PathVariable String field) {
+        if (bookService.getAll() != null && !bookService.getAll().isEmpty()) {
+            Pageable pageable = PageRequest.of(no, 3, Sort.by(Sort.Direction.ASC,
+                    field));
+            return new ResponseEntity<>(bookService.getAll(pageable), HttpStatus.OK);
+        } else {
+            throw new NoSuchElementException();
+        }
     }
 
-    @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteBook(@PathVariable Long id) {
-        bookService.deleteBook(id);
+    @GetMapping("id/{id}")
+    public ResponseEntity<?> getById(@PathVariable Integer id) {
+        if (bookService.getById(id) != null) {
+            return new ResponseEntity<>(bookService.getById(id), HttpStatus.OK);
+        } else {
+            throw new NoSuchElementException();
+        }
     }
 
-    @PostMapping("/customers")
-    public ResponseEntity<String> createCustomer(@RequestBody Customer customer) {
-        return ResponseEntity.status(HttpStatus.CREATED).body("Created customer with name " + customer.getName());
+    @GetMapping("author")
+    public ResponseEntity<?> getByAuthor(@RequestParam String name) {
+        if (bookService.getByAuthor(name) != null &&
+                !bookService.getByAuthor(name).isEmpty()) {
+            return new ResponseEntity<>(bookService.getByAuthor(name), HttpStatus.OK);
+        } else {
+            throw new NoSuchElementException();
+        }
     }
+
+    @PutMapping("id/{id}")
+    public ResponseEntity<?> updateById(@PathVariable Integer id, @RequestBody BookDTO entry) {
+        BookDTO old = bookService.getById(id);
+        old.setIsbn(entry.getIsbn() != null ? entry.getIsbn() : old.getIsbn());
+        old.setPrice(entry.getPrice() != null ? entry.getPrice() : old.getPrice());
+        old.setAuthor(
+                entry.getAuthor() != null && !entry.getAuthor().equals("") ? entry.getAuthor() : old.getAuthor());
+        old.setTitle(entry.getTitle() != null && !entry.getTitle().equals("") ? entry.getTitle() : old.getTitle());
+        bookService.save(old);
+        return new ResponseEntity<>(old, HttpStatus.OK);
+    }
+
+    @DeleteMapping("id/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Integer id) {
+        try {
+            bookService.getById(id);
+        } catch (Exception e) {
+            throw new EntityNotFoundException();
+        }
+        bookService.deleteById(id);
+        return new ResponseEntity<>(true, HttpStatus.OK);
+    }
+
 }
