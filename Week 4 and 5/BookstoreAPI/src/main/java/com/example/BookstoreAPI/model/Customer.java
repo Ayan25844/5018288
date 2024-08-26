bf7d51d93e61b1e39961a09e68a643440d902b6d
Ayan Chattejee <ayan25844@gmail.com>
2024-08-26 17:08:41 +0530

    Update Customer.java

--- Week 4 and 5/BookstoreAPI/src/main/java/com/example/BookstoreAPI/model/Customer.java
+++ Week 4 and 5/BookstoreAPI/src/main/java/com/example/BookstoreAPI/model/Customer.java
@@ -1,10 +1,28 @@
-package com.example.BookstoreAPI.model;
+package com.example.BookstoreAPI.entity;
 
 import lombok.Data;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 @Data
+@Entity
+@Table(name = "Customer_Details")
+@NamedQueries({
+        @NamedQuery(name = "Customer.getByName", query = "SELECT c FROM Customer c WHERE c.name=:name")
+})
 public class Customer {
-    private Long id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(unique = true, nullable = false)
     private String name;
-    private String email;
+    @Column(nullable = false)
+    private String password;
 }
